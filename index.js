c973a01343b2cb47985e5ba207e7a07474a15408
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-24 21:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var obj = { prop: 1 };
 
-function updateObjectWithKeyAndValue(object, 'prop2', 2){
+function updateObjectWithKeyAndValue(object, key, value){
   Object.assign({obj}, {prop2:'2'});
   return obj;
 
